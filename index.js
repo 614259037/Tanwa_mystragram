101e9d189ca12ef1e89ffe58a70ba50eba117ff6
614259037 <614259037@webmail.npru.ac.th>
2020-10-03 22:56:34 +0700

    suc

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const restaurantRouter = require('./routes/restaurant');
 const router = require('./routes/index');
 const hbs = require('express-handlebars');
 const path = require('path');
-app.use(express.static(path.join(__dirname,'public')));
-app.engine('hbs',hbs({extname:'hbs'}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('hbs', hbs({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/api' , router);
+app.use('/', router);
 
-app.listen(3000, ()=>{
-console.log('Listening to port 3000');
-
-});
+app.listen(
+    PORT,
+    () => {
+        console.log(`Listening to port ${PORT}`);
+    }
+);
